b2cff08e20c318b522b3f8e714fd217f35219f75
CrankIsDev <stanguise4@gmail.com>
2021-11-04 15:21:10 +0100

    Modif again

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useEffect, useState} from 'react';
 import gsap from 'gsap';
 import Transition from '../components/transition';
@@ -65,72 +64,4 @@ const About = () => {
   )
 }
 
-=======
-import React, {useEffect, useState} from 'react';
-import gsap from 'gsap';
-import Transition from '../components/transition';
-
-const tl = gsap.timeline();
-
-const transitionAnimation = (completeAnimation) => {
-  tl.from('h3 span', 1.8, {
-    y: 100,
-    ease: 'power4.out',
-    delay: 1,
-    skewY: 7,
-    stagger: {
-      amount: 0.3
-    }
-  }).to('.transition-topLeft', 1, {
-    width: 0,
-    ease: 'expo.inOut',
-    delay: -.8,
-    stagger: {
-      amount: 0.4
-    }
-  }).to('.transition', 0, {
-    css:{ 
-      display: "none" 
-    },
-    onComplete: completeAnimation
-  })
-}
-
-const About = () => {
-   const [animationComplete, setAnimationComplete] = useState(false);
-
-   const completeAnimation = () => {
-     setAnimationComplete(true);
-   };
-
-  useEffect(() => {
-    transitionAnimation(completeAnimation);
-  }, [])
-
-  return (
-    <>
-    {animationComplete === false ? <Transition /> : ""}
-    <div className="page">
-      <div className="container">
-        <div className="row">
-        <h3>
-        <div className="about">
-          <span>Hey there ,</span>
-        </div>
-        </h3>
-        <div className="about">
-          <p>I'm a Junior Front-End Developer</p>
-          <p>Based near Paris (78)</p>
-          <p>I started coding at end of Summer (2019)</p>
-          <p>Was cooking for 14 years and decided to change my life cause i love to create and have funs with code</p>
-          <p>I'm also passionate about Cinema, Video-Games, Basket-Ball.</p>
-        </div> 
-        </div>
-      </div>
-    </div>
-    </>
-  )
-}
-
->>>>>>> 57d3f5bd97fd3ce86ab39575e46f22413f725924
 export default About;
--- src/pages/coming.js
+++ src/pages/coming.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useEffect, useState} from 'react';
 import gsap from 'gsap';
 import Transition from '../components/transition';
@@ -72,79 +71,4 @@ const Coming = () => {
   );
 }
 
-=======
-import React, {useEffect, useState} from 'react';
-import gsap from 'gsap';
-import Transition from '../components/transition';
-
-const tl = gsap.timeline();
-
-const transitionAnimation = (completeAnimation) => {
-  tl.from('h1 span', 1.8, {
-    y: 100,
-    ease: 'power4.out',
-    delay: 1,
-    skewY: 7,
-    stagger: {
-      amount: 0.3
-    }
-  }).to('.transition-topLeft', 1, {
-    width: 0,
-    ease: 'expo.inOut',
-    delay: -.8,
-    stagger: {
-      amount: 0.4
-    }
-  }).to('.transition', 0, {
-    css:{ 
-      display: "none" 
-    },
-    onComplete: completeAnimation
-  })
-}
-
-const Coming = () => {
-
-  const [animationComplete, setAnimationComplete] = useState(false);
-
-  const completeAnimation = () => {
-    setAnimationComplete(true);
-  };
-
- useEffect(() => {
-   transitionAnimation(completeAnimation);
- }, [])
-
-  return (
-    <>
-    {animationComplete === false ? <Transition /> : ""}
-    <div className="page">
-      <div className="container">
-        <div className="row">
-          <h1>
-            <div className="about">
-              <span>Coming Soon...</span>
-            </div>
-          </h1>
-          <div>
-            <p className="tune">Stay Tune</p>
-          </div>
-            <div className="lamp__wrap">
-              <div className="lamp">
-                <div className="cable"></div>
-                <div className="cover"></div>
-                <div className="in-cover">
-                <div className="bulb"></div>
-              </div>
-             <div className="light"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    </>
-  );
-}
-
->>>>>>> 57d3f5bd97fd3ce86ab39575e46f22413f725924
 export default Coming;
